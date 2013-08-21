5a5d807cbff235584d02032496bc9d3073881244
masarakki <masaki@hisme.net>
2013-08-22 04:21:10 +0900

    add ship model

--- lib/naka/models/ship.rb
+++ lib/naka/models/ship.rb
@@ -1,13 +1,31 @@
 module Naka
   module Models
     class Ship
-      attr_accessor :id, :ship_id, :lv, :hp
+      Hp = Struct.new(:now, :max) do
+        def damaged? ; now != max ; end
+        def danger? ; now * 2 <= max ; end
+      end
+
+      attr_accessor :id, :ship_id, :lv, :fuel, :bull, :hp, :repairs_in, :condition
+
       def initialize(options = {})
+        @hp = Hp.new
         options.each_pair do |key, value|
           key = key.to_s.gsub /api_/, ''
           send("#{key}=", value) if respond_to?("#{key}=")
         end
       end
+
+      def locked? ; @locked ; end
+      def damaged? ; @hp.damaged? ; end
+      def tired? ; @condition <= 30 ; end
+      def danger? ; tired? || hp.danger? ; end
+
+      def nowhp=(val) ; @hp.now = val ; end
+      def maxhp=(val) ; @hp.max = val ; end
+      def locked=(val) ; @locked = val == 1 ; end
+      def ndock_time=(val) ; @repairs_in = val / 1000 ; end
+      def cond=(val) ; @condition = val ; end
     end
   end
 end
--- spec/naka/models/ship_spec.rb
+++ spec/naka/models/ship_spec.rb
@@ -1,4 +1,29 @@
 require 'spec_helper'
 
 describe Naka::Models::Ship do
+  let(:ship) { Naka::Models::Ship.new(MultiJson.load(mock_file('models/ship/ship.json'))) }
+  subject { ship }
+  its(:id) { should == 23 }
+  its(:ship_id) { should == 119 }
+  its(:lv) { should == 68 }
+  describe :hp do
+    subject { ship.hp }
+    its(:now) { should == 39 }
+    its(:max) { should == 43 }
+    its(:damaged?) { should be_true }
+  end
+  its(:damaged?) { should be_true }
+  its(:danger?) { should be_false }
+  its(:fuel) { should == 20 }
+  its(:bull) { should == 44 }
+  its(:locked?) { should be_true }
+  its(:repairs_in) { should == 1870 }
+  its(:condition) { should == 49 }
+
+  describe Naka::Models::Ship::Hp do
+    describe :danger? do
+      it { expect(Naka::Models::Ship::Hp.new(11, 21)).not_to be_danger }
+      it { expect(Naka::Models::Ship::Hp.new(10, 20)).to be_danger }
+    end
+  end
 end
