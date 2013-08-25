d9803241c0d530b0751f482796eaf47136aef99b
masarakki <masaki@hisme.net>
2013-08-26 05:23:49 +0900

    receive result of mission

--- lib/naka/api/mission.rb
+++ lib/naka/api/mission.rb
@@ -4,6 +4,10 @@ module Naka
       def start_mission(deck_id, mission_id)
         api.post '/kcsapi/api_req_mission/start', api_deck_id: deck_id, api_mission_id: mission_id
       end
+
+      def mission_result(deck_id)
+        api.post 'kcsapi/api_req_mission/result', api_deck_id: deck_id
+      end
     end
   end
 end
