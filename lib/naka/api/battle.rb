ac511032f92588e14b30d43fddc4d8951faef00c
masarakki <masaki@hisme.net>
2013-09-10 02:23:15 +0900

    add move model

--- lib/naka/api/battle.rb
+++ lib/naka/api/battle.rb
@@ -7,7 +7,8 @@ module Naka
       end
 
       def start(area_id, area_no, deck_id = 1, formation_id = 1)
-        request '/kcsapi/api_req_map/start', api_formation_id: formation_id, api_deck_id: deck_id, api_maparea_id: area_id, api_mapinfo_no: area_no
+        response = request '/kcsapi/api_req_map/start', api_formation_id: formation_id, api_deck_id: deck_id, api_maparea_id: area_id, api_mapinfo_no: area_no
+        Naka::Models::Battle::Move.from_api(response)
       end
 
       def battle(formation = 1)
@@ -23,7 +24,8 @@ module Naka
       end
 
       def next
-        request '/kcsapi/api_req_map/next'
+        response = request '/kcsapi/api_req_map/next'
+        Naka::Models::Battle::Move.from_api(response)
       end
     end
 
