410f11c50fb02f52005e4c1e9bb416df4c8014b0
masarakki <masaki@hisme.net>
2013-09-10 14:40:36 +0900

    add quest

--- lib/naka/strategies/battle.rb
+++ lib/naka/strategies/battle.rb
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 =begin
+201: 敵艦隊を撃破
+210: 敵艦隊10回
+211: 敵空母3隻
 213: 海上通商破壊
 214: あ号
+216: 敵艦隊主力
+218: 敵補給艦3隻
 220: い号
+221: ろ号
 226: 南海諸島制海権
 228: 海上護衛戦
 230: 潜水艦
@@ -11,7 +17,7 @@
 module Naka
   module Strategies
     class Battle < Base
-      quest_ids 213, 214, 220, 226, 228, 230
+      quest_ids 201, 210, 211, 213, 214, 216, 218, 220, 221, 226, 228, 230
 
       def initialize(user, map_id, area_id)
         @user = user
