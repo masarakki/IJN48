4072398cf7e2f634572843a3fbe39548f0f98202
masarakki <masaki@hisme.net>
2013-10-21 05:48:33 +0900

    change iron to steel

--- lib/naka/models/master/mission.rb
+++ lib/naka/models/master/mission.rb
@@ -3,7 +3,7 @@ module Naka
     module Master
       class Mission < Naka::Models::Base
 
-        Reward = Struct.new(:fuel, :bullet, :iron, :bauxite, :repair)
+        Reward = Struct.new(:fuel, :bullet, :steel, :bauxite, :repair)
         Cost = Struct.new(:fuel, :bullet)
 
         attr_accessor :time, :reward, :cost, :fleet
@@ -28,20 +28,20 @@ end
 
 Naka::Models::Master::Mission.setup do
   register id:  1, time:  15, cost: [0.3, 0.0], reward: {bullet: 30}, fleet: {any: 2}
-  register id:  2, time:  30, cost: [0.5, 0.0], reward: {bullet: 100, iron: 30, repair: 1}, fleet: {any: 4}
-  register id:  3, time:  20, cost: [0.3, 0.2], reward: {fuel: 30, bullet: 30, iron: 40}, fleet: {any: 3}
+  register id:  2, time:  30, cost: [0.5, 0.0], reward: {bullet: 100, steel: 30, repair: 1}, fleet: {any: 4}
+  register id:  3, time:  20, cost: [0.3, 0.2], reward: {fuel: 30, bullet: 30, steel: 40}, fleet: {any: 3}
   register id:  4, time:  50, cost: [0.5, 0.0], reward: {bullet: 60, repair: 1}, fleet: {cl: 1, dd: 2}
-  register id:  5, time:  90, cost: [0.5, 0.0], reward: {fuel: 200, bullet: 200, iron: 20, bauxite: 20}, fleet: {cl: 1, dd: 2, any: 1}
+  register id:  5, time:  90, cost: [0.5, 0.0], reward: {fuel: 200, bullet: 200, steel: 20, bauxite: 20}, fleet: {cl: 1, dd: 2, any: 1}
   register id:  6, time:  40, cost: [0.3, 0.2], reward: {bauxite: 80}, fleet: {any: 4}
-  register id:  7, time:  60, cost: [0.5, 0.0], reward: {iron: 50, bauxite: 30}, fleet: {any: 6}
-  register id:  8, time: 180, cost: [0.5, 0.2], reward: {fuel: 50, bullet: 100, iron: 50, bauxite: 50}, fleet: {any: 6}
+  register id:  7, time:  60, cost: [0.5, 0.0], reward: {steel: 50, bauxite: 30}, fleet: {any: 6}
+  register id:  8, time: 180, cost: [0.5, 0.2], reward: {fuel: 50, bullet: 100, steel: 50, bauxite: 50}, fleet: {any: 6}
   register id:  9, time: 240, cost: [0.5, 0.0], reward: {fuel: 350, repair: 2}, fleet: {cl: 1, dd:2, any: 1}
   register id: 10, time:  90, cost: [0.3, 0.0], reward: {bullet: 50, bauxite: 30, repair: 1}, fleet: {cl: 2, any: 1}
   register id: 11, time: 300, cost: [0.5, 0.0], reward: {bauxite: 250, repair: 1}, fleet: {dd: 2, any: 2}
-  register id: 12, time: 480, cost: [0.5, 0.0], reward: {fuel: 50, bullet: 250, iron: 200, bauxite: 50}, fleet: {dd: 2, any: 2}
+  register id: 12, time: 480, cost: [0.5, 0.0], reward: {fuel: 50, bullet: 250, steel: 200, bauxite: 50}, fleet: {dd: 2, any: 2}
   register id: 13, time: 240, cost: [0.5, 0.4], reward: {fuel: 240, bullet: 300, repair: 2}, fleet: {cl: 1, dd:4, any: 1}
-  register id: 14, time: 360, cost: [0.5, 0.0], reward: {bullet: 240, iron: 200, repair: 1}, fleet: {cl: 1, dd: 3, any: 2}
-  register id: 15, time: 720, cost: [0.5, 0.4], reward: {iron: 300, bauxite: 400}, fleet: {cl: 2, dd: 2, any: 2}
-  register id: 16, time: 900, cost: [0.5, 0.4], reward: {fuel: 500, bullet: 500, iron: 200, bauxite: 200, repair: 2}, fleet: {cl: 1, dd: 2, any: 3}
+  register id: 14, time: 360, cost: [0.5, 0.0], reward: {bullet: 240, steel: 200, repair: 1}, fleet: {cl: 1, dd: 3, any: 2}
+  register id: 15, time: 720, cost: [0.5, 0.4], reward: {steel: 300, bauxite: 400}, fleet: {cl: 2, dd: 2, any: 2}
+  register id: 16, time: 900, cost: [0.5, 0.4], reward: {fuel: 500, bullet: 500, steel: 200, bauxite: 200, repair: 2}, fleet: {cl: 1, dd: 2, any: 3}
 
 end
--- spec/naka/models/master/mission_spec.rb
+++ spec/naka/models/master/mission_spec.rb
@@ -5,7 +5,7 @@ describe Naka::Models::Master::Mission do
     subject { Naka::Models::Master::Mission.find(1) }
     its(:id) { should == 1 }
     its("cost.to_h") { should == {fuel: 0.3, bullet: 0}}
-    its("reward.to_h") { should == {fuel: 0, bullet: 30, iron: 0, bauxite: 0, repair: 0}}
+    its("reward.to_h") { should == {fuel: 0, bullet: 30, steel: 0, bauxite: 0, repair: 0}}
     its(:time) { should == 15 }
     its(:fleet) { should == {any: 2} }
   end
