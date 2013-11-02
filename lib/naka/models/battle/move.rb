e7830f190b49810cf229c1e5d5f3c85491424cd3
masarakki <masaki@hisme.net>
2013-11-02 21:28:59 +0900

    forget commit...

--- lib/naka/models/battle/move.rb
+++ lib/naka/models/battle/move.rb
@@ -2,7 +2,7 @@ module Naka
   module Models
     module Battle
       class Move
-        attr_accessor :cell_id, :has_next, :boss_cell_id, :has_battle, :battle_type, :midnight_start, :night_to_day
+        attr_accessor :cell_id, :has_next, :boss_cell_id, :has_battle, :battle_type, :midnight_start, :night_to_day, :color
 
         def self.from_api(response)
           move = new
@@ -11,9 +11,10 @@ module Naka
           move.boss_cell_id = data[:api_bosscell_no]
           move.has_next = data[:api_next] != 0
           move.has_battle = data.has_key?(:api_enemy)
+          move.color = data[:api_color_no]
           move.battle_type = case data[:api_color_no]
                              when 6 then :midnight_start
-                             when 8 then :night_to_day
+                             when 7, 8 then :night_to_day
                              else :normal
                              end
           move
@@ -21,7 +22,7 @@ module Naka
 
         def battle? ; has_battle ; end
         def skippable? ; !battle? ; end
-        def boss? ; boss_cell_id == cell_id || night_to_day? ; end
+        def boss? ; boss_cell_id == cell_id || color == 8 ; end
         def terminal? ; ! has_next ; end
         def midnight? ; battle_type == :midnight_start ; end
         def night_to_day? ; battle_type == :night_to_day ; end
