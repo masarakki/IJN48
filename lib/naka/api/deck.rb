0d32445cff4b69d4e8173d9530a3c18e31faca12
masarakki <masaki@hisme.net>
2013-11-07 07:21:01 +0900

    remove unnessesary request

--- lib/naka/api/deck.rb
+++ lib/naka/api/deck.rb
@@ -5,13 +5,11 @@ module Naka
       # index: zero origin
       def change(deck_id, index, ship_id)
         request '/kcsapi/api_req_hensei/change', api_ship_id: ship_id, api_ship_idx: index, api_id: deck_id
-        request '/kcsapi/api_get_member/deck'
       end
 
       # index: zero origin
       def remove(deck_id, index)
         change(deck_id, index, -1)
-        request '/kcsapi/api_get_member/deck'
       end
 
       def organize(fleet_id, ships)
